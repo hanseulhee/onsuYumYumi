8606fe84519acf93d308834010b12b009c73197d
hanseulhee <3021062@gmail.com>
2022-02-19 14:22:43 +0900

    [#2] theme style apply

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import App from "components/App";
+import { ThemeProvider } from "@emotion/react";
+import theme from "styles/theme";
 import GlobalStyle from "styles/GlobalStyle";
 
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyle />
     <App />
-  </>,
+  </ThemeProvider>,
   document.getElementById("root")
 );
