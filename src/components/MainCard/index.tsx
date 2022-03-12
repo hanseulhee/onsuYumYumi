605e25f1055e84de8949d2ad5055e3596c680f5c
hanseulhee <3021062@gmail.com>
2022-03-13 01:47:38 +0900

    [#31] apply mainCard

--- src/components/MainCard/index.tsx
+++ src/components/MainCard/index.tsx
@@ -13,13 +13,13 @@ interface Props {
 function MainCard({ name, summary, img }: Props) {
   return (
     <Link to={`/detail/${name}`}>
-    <div css={CardTool}>
-      <div css={CardContent}>
-        <span css={CardTitle}>{name}</span>
-        <span css={CardSummary}>{summary}</span>
-        <img src={img} alt="restaurant" css={CardImg} />
+      <div css={CardTool}>
+        <div css={CardContent}>
+          <span css={CardTitle}>{name}</span>
+          <span css={CardSummary}>{summary}</span>
+          <img src={img} alt="restaurant" css={CardImg} />
+        </div>
       </div>
-    </div>
     </Link>
   );
 }
@@ -38,22 +38,33 @@ const CardTool = (theme: Theme) => css`
   border: 1px solid #f1f1f1;
   border-radius: 3px;
   cursor: pointer;
+
+  ${theme.mediaQuery.mobile} {
+    height: 220px;
+  }
 `;
 
-const CardContent = css`
+const CardContent = (theme: Theme) => css`
   position: absolute;
   display: flex;
   flex-direction: column;
-  padding: 10px 10px;
+  padding: 10px 0px;
   height: 350px;
+  ${theme.mediaQuery.mobile} {
+    height: 230px;
+  }
 `;
 
-const CardImg = css`
+const CardImg = (theme: Theme) => css`
   width: 230px;
   height: 225px;
   border-radius: 2px;
   box-shadow: rgba(0, 0, 0, 0.16) 1px 1px 5px;
   margin-top: 10px;
+  ${theme.mediaQuery.mobile} {
+    width: 145px;
+    height: 140px;
+  }
 `;
 
 const CardTitle = (theme: Theme) => css`
@@ -61,7 +72,7 @@ const CardTitle = (theme: Theme) => css`
   font-size: 1.24rem;
 
   ${theme.mediaQuery.mobile} {
-    font-size: 1.063rem;
+    font-size: 1.11rem;
   }
 `;
 
@@ -69,4 +80,7 @@ const CardSummary = (theme: Theme) => css`
   font-weight: ${theme.fontWeight.light};
   font-size: 0.92rem;
   color: #8b95a1;
+  ${theme.mediaQuery.mobile} {
+    font-size: 0.72rem;
+  }
 `;
